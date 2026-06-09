41b0e33578e746d21ee5f8a5a28efc0044e14964
brianbayona <brianmauriciobayonaravelo@gmail.com>
2026-06-09 17:28:43 -0500

    endpoint de la api actualizado con el ip

--- client/js/api/tareasApi.js
+++ client/js/api/tareasApi.js
@@ -28,7 +28,7 @@
 //   - Histórico:     antes apuntaba a :3005 (puerto incorrecto,
 //                     provocaba "Error de conexión" en la UI).
 
-const API_URL = "http://localhost:3002";
+const API_URL = "http://10.5.225.71:3002";
 
 
 // fetchUsers()
